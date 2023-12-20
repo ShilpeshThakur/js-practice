10c1f88f3ccbf82bf310b5dfc32d89b85cb3730b
ShilpeshThakur <44371498+ShilpeshThakur@users.noreply.github.com>
2023-12-20 12:21:41 +0000

    datatype summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,24 @@
+// primitive
+
+// 7 types : string, Number, Boolean, Null, Undefined, symbol, BigInt
+
+// JavaScript is dynamically typed language.This means that variable types are determined at runtime, and you do not need to explicitly declare tthe type of a variable before using it, You can assign different types of values to a variable during its lifetime.
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail; //undefined
+
+const id = symbol('123')
+const anotheId = symbol('123')
+
+console.log(id === anotheId);
+
+const bigNumber = 34554555455544
+
+// reference (non primitive)
+
+// Array, Objects, Functions
+
