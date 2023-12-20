bc62188a9ee40a2c6e7f79d1b39531c8423fc312
ShilpeshThakur <44371498+ShilpeshThakur@users.noreply.github.com>
2023-12-20 15:26:23 +0000

    date first commit

---
+++ 01_basics/07_dates_in_js.js
@@ -0,0 +1,24 @@
+// Dates
+
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+
+// console.log(typeof myDate);
+
+let myCreatedDate = new Date(2023,0,23,);
+// console.log(myCreatedDate.toDateString());
+
+let myCreatedDate2 = new Date(2023,0,23, 5, 3);
+// console.log(myCreatedDate2.toLocaleString());
+
+let myCreatedDate3 = new Date("2023-01-14");
+console.log(myCreatedDate3.toLocaleString());
+
+let myCreatedDate4 = new Date("01-14-2023");
+console.log(myCreatedDate4.toLocaleString());
+
