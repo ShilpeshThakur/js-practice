955ac2a333eda4e4f191f7168fc915f0b1abff1b
ShilpeshThakur <44371498+ShilpeshThakur@users.noreply.github.com>
2023-12-20 15:11:35 +0000

    nums and math

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,38 @@
+const score = 100;
+// console.log(score);
+
+const balance = new Number(100);
+// console.log(balance);
+
+// console.log(score.toString());
+// console.log(score.toFixed(2));
+
+const otherNumber = 123.8966
+
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// ******************* Maths ************************
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.5));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.8));
+// console.log(Math.min(4,8,5,25));
+// console.log(Math.max(4,8,5,25));
+
+// console.log(Math.random());
+// console.log((Math.random() * 10) + 1);
+// console.log(Math.floor(Math.random() * 10) + 1);
+
+
+const min = 10
+const max = 20
+
+const val = Math.random();
+console.log(val);
+console.log(Math.floor( val * (max - min + 1)) + min)
+
